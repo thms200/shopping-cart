0c8933691fe4b0777db37277daa6ecd8bafc59ef
thms200 <thms0504@gmail.com>
2020-05-28 18:50:27 +0900

    refactor: Fix git conflict

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,6 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import { createGlobalStyle } from 'styled-components';
-=======
-import { createGlobalStyle } from 'styled-components'
->>>>>>> e18c012aee1be1136c68ef08c1b489124c98d3c9
 import Cart from './Cart';
 import Item from './Item';
 import Discount from './Discount';
